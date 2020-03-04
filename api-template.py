f3f3f895b8ac79dd90bc9e5e2f3dcc84baaab6d2
Sri Julapally <sripushkar@gmail.com>
2020-03-03 23:01:13 -0500

    added api template file for testing purposes

---
+++ api-template.py
@@ -0,0 +1,10 @@
+#TEMPORARY API FILE
+import praw
+
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+USERNAME = ""
+PASSWORD = ""
+USER_AGENT = ""
+
+reddit = praw.Reddit(client_id = CLIENT_ID, client_secret = CLIENT_SECRET, password = PASSWORD, user_agent = USER_AGENT, username = USERNAME)
